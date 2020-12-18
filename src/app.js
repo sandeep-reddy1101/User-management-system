f33f5600dd0e753cc6c7c64fb16a4ef49b4bbdd6
Sandeep Reddy <sandeep.x.sandy49@gmail.com>
2020-12-18 20:26:30 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use('/', router);
 app.use('/friend', chatRouter);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`App started at port ${port}!`)
 });
